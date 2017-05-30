f2dee4e2710640eeca93eeb34d8b981d48e7ece5
Brian Mckie <brianmckie2@gmail.com>
2017-05-30 19:49:05 -0400

    Working on refactoring code

--- src/index.js
+++ src/index.js
@@ -4,82 +4,115 @@ import App from './App';
 import './index.css';
 import axios from 'axios';
 
-let WikiItem = React.createClass({
-	render: function(){
-		return(
-			<div className="singleItems">
-				<li>
-					<a href={this.props.url}>{this.props.title}</a>
-					<p>{this.props.desc}</p>
-				</li>
-			</div>
-		)
-	}
-})
 
-let WikiComponent = React.createClass({
-  getInitialState: function(){
-    return {
-      output: []
-    }
-  },
-  componentDidMount: function(){
-    axios.get('https://en.wikipedia.org//w/api.php?action=opensearch&format=json&origin=*&search=atlanta&limit=10').then(response => {
-      console.log(response.data)
-        this.setState({
-          output: response.data
-        })
-    })
-  },
- 	render: function(){
- 		console.log('before state' + this.state.output)
- 		var output = this.state.output;
- 		output = output.map(function(title, index){
- 			return(
- 				<WikiItem key={index} title={output[1][index]}/>
- 			);
- 		});
-    console.log('after state' + this.state.output)
-    return(
-         <div className="row mainsection">
-         	<div className="col-md-4 col-md-offset-4">
-	         	<form>
-	         		<p>
-		           <input type="text" className="searchQuery" ref="search" onKeyDown={this.tenEntries}/>
-		           <span className="glyphicon glyphicon-search"></span>
-		          </p>
-	          </form>
-	          <RandomEntryComponent />
-	         <ul>
-	         	{output}
-	         </ul>
-           </div>
-         </div>
-        );
-    },//render
- tenEntries: function(e){
-   const userInput = this.refs.search.value;
-  if (e.keyCode === 13){
-  	e.preventDefault();
-    this.setState({
-      userInput: e.target.value
-      //console.log(userInput);
-    });
+class InputForm extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      searchTerm: ''
+    };
+  }
   
+  handleInputChange(e){
+    this.setState({
+      searchTerm: e.target.value
+    });
   }
+  
+  render(){
+    return(
+       <div className="row mainsection">
+            <div className="col-md-4 col-md-offset-4">
+            <form>
+              <p>
+               <input type="text" className="searchQuery" ref="search" onKeyDown={this.handleUserInput}/>
+               <span className="glyphicon glyphicon-search"></span>
+              </p>
+            </form>
+              <div className="randomArticle">
+          <a href="https://en.wikipedia.org/wiki/Special:Random" target="_blank" id="random">Random article</a>
+            </div>
+     
+        </div>
+          </div>
+    )
+  }
+
+
+
+
+
+
 }
-});
+//  render: function(){
+//    return(
+//      <div className="singleItems">
+//        <li>
+//          <a href={this.props.url}>{this.props.title}</a>
+//          <p>{this.props.desc}</p>
+//        </li>
+//      </div>
+//    )
+//  }
+// }
 
-let RandomEntryComponent = React.createClass({
-  render: function(){
-    return (
-    	<div className="randomArticle">
-   			<a href="https://en.wikipedia.org/wiki/Special:Random" target="_blank" id="random">Random article</a>
-   		</div>
-    );
-  }
-});
+// let WikiComponent = React.createClass({
+//   getInitialState: function(){
+//     return {
+//       output: ['',[],[],[]],
+//       userInput: ''
+//     };
+//   },
+
+  
+//   componentDidMount: function(){
+//     //var search = this.refs.search.value;
+//     axios.get('https://en.wikipedia.org//w/api.php?action=opensearch&format=json&origin=*&search='+userInput+'&limit=10').then(response => {
+//       console.log(response.data)
+//         this.setState({
+//           output: response.data
+//         })
+//     })
+//   },
+//    render: function(){
+//      //console.log('before state' + this.state.output)
+//      var output = this.state.output;
+//      output = output.map(function(title, index){
+//        return(
+//          <WikiItem key={index} title={output[1][index]}/>
+//        );
+//      });
+//     // console.log('after state' + this.state.output)
+//     return(
+//          <div className="row mainsection">
+//            <div className="col-md-4 col-md-offset-4">
+//            <form>
+//              <p>
+//               <input type="text" className="searchQuery" ref="search" onKeyDown={this.handleUserInput}/>
+//               <span className="glyphicon glyphicon-search"></span>
+//              </p>
+//            </form>
+//              <div className="randomArticle">
+//          <a href="https://en.wikipedia.org/wiki/Special:Random" target="_blank" id="random">Random article</a>
+//        </div>
+//     {WikiItem}
+//        </div>
+//          </div>
+//         );
+//     },
+//      handleUserInput: function(e){
+//      var search;
+//      console.log(search)
+//      if(e.keyCode === 13){
+//        e.preventDefault();
+//        //search = this.refs.search.value
+//        this.setState({userInput: e.target.value});
+//        console.log('after UserInput state' + this.state.userInput);
+//      }
+//    },
+//   //render
+
+// });
 
 
-
-ReactDOM.render(<WikiComponent />, document.getElementById('entryList'));
+// ReactDOM.render(<WikiComponent />, document.getElementById('entryList'));
